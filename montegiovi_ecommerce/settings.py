ebb6e0ca9c60b2fc63a7e77ca536fdba62529b89
Baal89 <efestos89@gmail.com>
2020-10-06 14:54:40 +0000

    Deployment postgress heroku

--- montegiovi_ecommerce/settings.py
+++ montegiovi_ecommerce/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 import env
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,10 +125,11 @@ WSGI_APPLICATION = 'montegiovi_ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+ }
+
 
 
 # Password validation
